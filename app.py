31fc0c35d4e081cf6cee45b203d509064579442f
Chenchen Zhang <44817576+ChenchenZhangEvette@users.noreply.github.com>
2019-07-13 18:07:59 -0400

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
 app.config['MYSQL_PASSWORD'] = 'mysql'
-app.config['MYSQL_DB'] = 'sys'
+app.config['MYSQL_DB'] = 'statestreet'
 
 mysql = MySQL(app)
 
